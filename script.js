440ccce96e77c1875d9a75448a0b0aa72a227452
Hassan Raza <hr.raza04@gmail.com>
2025-08-12 11:02:26 +0530

    js queston frame

---
+++ script.js
@@ -0,0 +1,38 @@
+const questions = [ 
+    {
+        question:"Which is the largest animal in the world",
+        answers:[
+            {text:"shark", correct:false},
+            {text:"Blue Whale", correct:true},
+            {text:"Elephant", correct:false},
+            {text:"Girafee", correct:false},
+        ]
+    },
+    {
+        question:"Which is the smallest country in the world",
+        answers:[
+            {text:"Vatican City", correct:true},
+            {text:"Bhutan", correct:false},
+            {text:"Nepal", correct:false},
+            {text:"Sri Lanka", correct:false},
+        ]
+    },
+    {
+        question:"Which is the largest desert in the world",
+        answers:[
+            {text:"Kalahari", correct:false},
+            {text:"Gobhi", correct:false},
+            {text:"Sahara", correct:false},
+            {text:"Antarctica", correct:true},
+        ]
+    },
+    {
+        question:"Which is the smallest continent  in the world",
+        answers:[
+            {text:"Asia", correct:false},
+            {text:"Austallia", correct:true},
+            {text:"Arctic", correct:false},
+            {text:"Africa", correct:false},
+        ]
+    },
+]
